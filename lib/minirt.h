c2834eff1f0b6ef9b7ca0a9505aada280fcccc8d
jstudnic1 <studnicka.jakub04@gmail.com>
2024-06-07 10:16:31 +0200

    vec oper done

--- lib/minirt.h
+++ lib/minirt.h
@@ -6,22 +6,27 @@
 /*   By: jstudnic <studnicka.jakub04@gmail.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 17:47:15 by jstudnic          #+#    #+#             */
-/*   Updated: 2024/06/06 17:52:31 by jstudnic         ###   ########.fr       */
+/*   Updated: 2024/06/07 10:09:32 by jstudnic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINIRT_H
 # define MINIRT_H
 
-#include <math.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdbool.h>
-#include <fcntl.h>
+# include "../mlx/mlx.h"
+# include "vector.h"
+# include <fcntl.h>
+# include <math.h>
+# include <stdbool.h>
+# include <stdio.h>
+# include <unistd.h>
 
-#include "../mlx/mlx.h"
-#include "vector.h"
+/* VECTOR UTILS*/
+double		vec_len(t_vector vector);
+double		dot_product(t_vector u, t_vector v);
+t_vector	cross_product(t_vector u, t_vector v);
+double		vec_len2(t_vector vector);
 
-
+/* VECTORS */
 
 #endif
--- srcs/vectors_utils.c
+++ srcs/vectors_utils.c
@@ -6,7 +6,7 @@
 /*   By: jstudnic <studnicka.jakub04@gmail.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 14:16:23 by jstudnic          #+#    #+#             */
-/*   Updated: 2024/06/06 18:07:37 by jstudnic         ###   ########.fr       */
+/*   Updated: 2024/06/07 10:16:00 by jstudnic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,27 @@ double	dot_product(t_vector u, t_vector v)
 	return (u.x * v.x + u.y * v.y + u.z * v.z);
 }
 
-double cross_product(t_vector u, t_vector v)
+t_vector	cross_product(t_vector u, t_vector v)
 {
-	t_vector	res;
+	t_vector	cross;
 
-	return t_vector()
+	cross.x = u.y * v.z - u.z * v.y;
+	cross.y = u.z * v.x - u.x * v.z;
+	cross.z = u.x * v.y - u.y * v.x;
+	return (cross);
+}
+
+double	vec_len2(t_vector vector)
+{
+	return (vector.x * vector.x + vector.y * vector.y + vector.z * vector.z);
+}
+
+t_vector	create_vector(double x, double y, double z)
+{
+	t_vector	vector;
+
+	vector.x = x;
+	vector.y = y;
+	vector.z = z;
+	return (vector);
 }
